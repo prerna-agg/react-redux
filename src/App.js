3252eec278ceb80e6e154dd88239f5e61daaa693
prerna <paruprerna@gmail.com>
2020-04-26 18:48:35 +0530

    controlled forms and form validations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter>
