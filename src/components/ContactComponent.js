108c2d84e26f07a48dceae932aa560a447068920
prerna <paruprerna@gmail.com>
2020-04-27 14:06:26 +0530

    Corrected React Redux Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -26,6 +26,7 @@ class Contact extends Component {
 	handleSubmit(values) {
 		console.log('Current State is: ' + JSON.stringify(values));
 		alert('Current State is: ' + JSON.stringify(values));
+		// event.preventDefault();
 	}
 
 	render() {
@@ -213,6 +214,30 @@ class Contact extends Component {
 								</Col>
 							</Row>
 
+							<Row className='form-group'>
+								<Col md={{ size: 6, offset: 2 }}>
+									<div className='form-check'>
+										<Label check>
+											<Control.checkbox
+												model='.agree'
+												name='agree'
+												className='form-check-input'
+											/>{' '}
+											<strong>May we contact you?</strong>
+										</Label>
+									</div>
+								</Col>
+								<Col md={{ size: 3, offset: 1 }}>
+									<Control.select
+										model='.contactType'
+										name='contactType'
+										className='form-control'
+									>
+										<option>Tel.</option>
+										<option>Email</option>
+									</Control.select>
+								</Col>
+							</Row>
 							<Row className='form-group'>
 								<Label htmlFor='message' md={2}>
 									Your Feedback
