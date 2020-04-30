2bba8898d062b68d39881ef22b2737688c6495e5
prerna <paruprerna@gmail.com>
2020-04-30 23:32:22 +0530

    Fetch from Server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,16 +8,17 @@ import {
 	CardSubtitle,
 } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({ item, isLoading, errMess }) {
 	if (isLoading) {
 		return <Loading />;
 	} else if (errMess) {
 		return <h4>{errMess}</h4>;
-	} else
+	} else {
 		return (
 			<Card>
-				<CardImg src={item.image} alt={item.name} />
+				<CardImg src={baseUrl + item.image} alt={item.name} />
 				<CardBody>
 					<CardTitle>{item.name}</CardTitle>
 					{item.designation ? (
@@ -27,6 +28,7 @@ function RenderCard({ item, isLoading, errMess }) {
 				</CardBody>
 			</Card>
 		);
+	}
 }
 
 function Home(props) {
@@ -37,11 +39,15 @@ function Home(props) {
 					<RenderCard
 						item={props.dish}
 						isLoading={props.dishesLoading}
-						errMess={props.dishesErrMess}
+						errMess={props.dishErrMess}
 					/>
 				</div>
 				<div className='col-12 col-md m-1'>
-					<RenderCard item={props.promotion} />
+					<RenderCard
+						item={props.promotion}
+						isLoading={props.promoLoading}
+						errMess={props.promoErrMess}
+					/>
 				</div>
 				<div className='col-12 col-md m-1'>
 					<RenderCard item={props.leader} />
